b8c63b52f783c310f1759ff0513a497d6e43e74a
Austin-GPSFMS <austin@gpsfms.com>
2025-04-30 16:06:21 -0400

    Delete gpsfms-camera-link/app directory

--- gpsfms-camera-link/app/cameraButton.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-geotab.customButtons = geotab.customButtons || {};
-geotab.customButtons.cameraSolutionsButton = function(event, api, state) {
-  event.preventDefault();
-  window.open('https://www.gpsfms.com/geotab-camera-solutions', '_blank');
-};
